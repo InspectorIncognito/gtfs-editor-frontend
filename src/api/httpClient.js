56eb2932435bb7eb1a271f2f1b17ec255658f734
cephe <cephei.1313@gmail.com>
2024-09-21 11:47:14 +0200

    Add language headers and request interceptor
    Updated the HTTP client to include "Content-Language" and "Accept-Language" headers, retrieving values from localStorage. Also introduced a request interceptor to dynamically set these headers on each request.

--- src/api/httpClient.js
+++ src/api/httpClient.js
@@ -1,47 +1,59 @@
 import axios from 'axios';
 
+import {LANGUAGE_KEY} from '@/utils/consts.js';
+
 const httpClient = axios.create({
-    baseURL: process.env.VUE_APP_BASE_URL,
-    timeout: 5000, // indicates, 1000ms ie. 1 second
-    headers: {
-        'User-Id': '',
-        'User-Token': '',
-        "Content-Type": "application/json",
-    }
+  baseURL: process.env.VUE_APP_BASE_URL,
+  timeout: 5000, // indicates, 1000ms ie. 1 second
+  headers: {
+    'User-Id': '',
+    'User-Token': '',
+    "Content-Type": "application/json",
+    "Content-Language": window.localStorage.getItem(LANGUAGE_KEY),
+    "Accept-Language": window.localStorage.getItem(LANGUAGE_KEY)
+  }
 });
 
-httpClient.getBaseURL = ()=>{
-    return process.env.VUE_APP_BASE_URL;
+httpClient.getBaseURL = () => {
+  return process.env.VUE_APP_BASE_URL;
 }
 
 // interceptor to catch errors
 const errorInterceptor = error => {
-    // all the error responses
-    switch(error.response.status) {
-        case 400:
-            console.error(error.response.status, error.message);
-            break;
+  // all the error responses
+  switch (error.response.status) {
+    case 400:
+      console.error(error.response.status, error.message);
+      break;
 
-        default:
-            console.error(error.response.status, error.message);
-    }
-    return Promise.reject(error);
+    default:
+      console.error(error.response.status, error.message);
+  }
+  return Promise.reject(error);
 }
 
 // Interceptor for responses
 const responseInterceptor = response => {
-    switch(response.status) {
-        case 200: 
-            // yay!
-            break;
-        // any other cases
-        default:
-            // default case
-    }
+  switch (response.status) {
+    case 200:
+      // yay!
+      break;
+    // any other cases
+    default:
+    // default case
+  }
 
-    return response;
+  return response;
+}
+
+const requestInterceptor = config => {
+  config.headers["Accept-Language"] = window.localStorage.getItem(LANGUAGE_KEY);
+  config.headers["Content-Language"] = window.localStorage.getItem(LANGUAGE_KEY);
+
+  return config;
 }
 
 httpClient.interceptors.response.use(responseInterceptor, errorInterceptor);
+httpClient.interceptors.request.use(requestInterceptor);
 
 export default httpClient;
