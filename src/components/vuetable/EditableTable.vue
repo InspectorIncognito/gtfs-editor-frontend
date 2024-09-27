0a47b180b23a2e8215017effd2cd1ec4b557b5b1
cephe <cephei.1313@gmail.com>
2024-09-27 12:23:18 +0200

    Fix placeholder text in editable table error div

--- src/components/vuetable/EditableTable.vue
+++ src/components/vuetable/EditableTable.vue
@@ -59,7 +59,7 @@
                                       @vuetable-pagination:change-page="onChangePage">
           </VuetablePaginationDropDown>
         </div>
-        <div class="table-errors">aquí poner error global</div>
+        <div class="table-errors">-</div>
         <div class="table-footer-buttons">
           <button class="btn green" @click="saveChanges"><span>{{ $t('vuetable.save') }}</span></button>
           <slot name="additional-buttons"></slot>
