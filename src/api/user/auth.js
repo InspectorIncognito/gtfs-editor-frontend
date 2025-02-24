cd1cb6ec4441dae224cc5558f1149c32add574c1
Mrtn-fa <mnfaray@gmail.com>
2025-02-24 16:23:15 -0300

    fix(html): fixed user endpoints
    Fixed email confirmation endpoint
    Fixed password recover endpoint
    Fixed auth base endpoint (added '/')

--- src/api/user/auth.js
+++ src/api/user/auth.js
@@ -1,6 +1,6 @@
 import httpClient from "../httpClient";
 
-const BASE_END_POINT = 'user';
+const BASE_END_POINT = '/user';
 const END_POINT_LOGIN = `${BASE_END_POINT}/login/`;
 const END_POINT_LOGOUT = `${BASE_END_POINT}/logout/`;
 
